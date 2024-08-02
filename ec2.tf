f9586d90ae3c38afd670f8a7d9691f1fc6c1a764
Aramyd <aramyd@LAPTOP-I9MRGKMV.localdomain>
2024-08-01 21:23:18 -0500

    format code for alignment

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-   region = "eu-west-2"
+  region = "eu-west-2"
 }
 resource "aws_instance" "project-instance" {
-   ami = "ami-07d20571c32ba6cdc"
-   instance_type = "t2.micro"
+  ami           = "ami-07d20571c32ba6cdc"
+  instance_type = "t2.micro"
 }
