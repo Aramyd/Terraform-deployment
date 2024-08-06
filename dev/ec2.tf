b08facce8828ec2b18a7df42137715b176b05557
Aramyd <aramyd@LAPTOP-I9MRGKMV.localdomain>
2024-08-06 13:09:51 -0500

    Improve Terraform code

---
+++ dev/ec2.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "aramyd-tf-state-bucket"
+    key    = "terraform-state/dev-terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
+
+resource "aws_s3_bucket" "My-terraform-state-bucket" {
+  bucket = "aramyd-tf-state-bucket"
+}
+
+resource "aws_instance" "project-instance" {
+  ami           = "ami-07d20571c32ba6cdc"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "dev-webserver"
+  }
+}
+
+
--- ec2.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region = "eu-west-2"
-}
-resource "aws_instance" "project-instance" {
-  ami           = "ami-07d20571c32ba6cdc"
-  instance_type = "t2.micro"
-}
